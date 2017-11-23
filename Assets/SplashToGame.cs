a1f0881d74bd261e6abacf4e2c020c57f7a65d0f
Stephen Boynton <boyntonsc@gmail.com>
2017-11-23 18:31:49 -0500

    Splash

---
+++ Assets/SplashToGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashToGame : MonoBehaviour {
+    [SerializeField] int delaySeconds = 1;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadGame", delaySeconds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadGame() {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+}
