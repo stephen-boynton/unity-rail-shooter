cd7a6922763c85fdfad8cd7a92b32a2609698979
Stephen Boynton <boyntonsc@gmail.com>
2017-11-24 18:01:24 -0500

    Vertical movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
+    [SerializeField] float xRangeR = 6f;
+    [SerializeField] float xRangeL = 6f;
+    [SerializeField] float yRangeU = 6f;
+    [SerializeField] float yRangeD = 6f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +17,27 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        MoveX();
+        MoveY();
+    }
+
+    private void MoveX()
+    {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = transform.localPosition.x + xOffset;
-        float newXPos = Mathf.Clamp(rawNewXPos, -5, 7);
+        float newXPos = Mathf.Clamp(rawNewXPos, -xRangeL, xRangeR);
         transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void MoveY()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float newYPos = Mathf.Clamp(rawNewYPos, -yRangeD, yRangeU);
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPos, transform.localPosition.z);
     }
 }
